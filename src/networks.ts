c6edf33136f45bf5f7f26ce433e146a4d190cb91
tnrdd <tnrdd@tutanota.com>
2024-08-06 07:13:46 +0200

    feat: add arbitrum one support

--- src/networks.ts
+++ src/networks.ts
@@ -17,6 +17,23 @@ type Contract = {
 
 const networks: Network[] = [
   {
+    id: 42161,
+    name: "arbitrum-one",
+    rpc: process.env.RPC_URL_ARBITRUM_ONE ?? "https://arb1.arbitrum.io/rpc",
+    contracts: [
+      {
+        name: "Allo",
+        address: "0x1133eA7Af70876e64665ecD07C0A0476d09465a1",
+        fromBlock: 239835829n,
+      },
+      {
+        name: "AlloRegistry",
+        address: "0x4AAcca72145e1dF2aeC137E1f3C5E3D75DB8b5f3",
+        fromBlock: 239827023n,
+      },
+    ],
+  },
+  {
     id: 11155420,
     name: "optimism-sepolia",
     rpc:
